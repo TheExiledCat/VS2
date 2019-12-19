d20a5729bc1960d6b0b88a738e3094ec7cc9b257
yomo-kun <24764@ma-web.nl>
2019-12-19 11:59:39 +0100

    duealist and dodger

--- Assets/DEVS/Scripts/dodger.cs
+++ Assets/DEVS/Scripts/dodger.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Duelist : Enemy
+public class dodger : Enemy
 {
     // Start is called before the first frame update
     void Start()
